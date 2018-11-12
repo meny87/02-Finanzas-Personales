916f93ce55340fa3dc6157e892b123f63b8e568b
Emmanuel Casas <emmanuelcasas@MacBook-Pro-de-Emmanuel.local>
2018-11-11 20:51:48 -0600

    Adding mongoose config variables

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,17 @@
-var mongoose = require('mongoose');
+/*var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.set('useNewUrlParser', true);
 mongoose.connect("mongodb://localhost:27017/Finances",{useNewUrlParser: true });
+
+module.exports = {mongoose};*/
+
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser: true });
 
 module.exports = {mongoose};
